43687f9a82ac16d5619dbf867e3e32f7088b5e48
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2025-02-28 16:40:16 -0300

    updating treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-        local leader = require("config.keymap").leader
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { 
-                "c", 
-                "vim",
-                "lua",
-                "vimdoc",
-                "query",
-                "html",
-                "css",
-                "java",
-                "markdown",
-                "markdown_inline",
-                "json",
-                "angular",
-                "scss",
-                "javascript",
-                "typescript",
-                "tsx"
-            },
-            auto_install = true,
-            highlight = {
-                enable = true,
-            },
-            incremental_selection = { 
-                enable = true,
-                keymaps = { 
-                    -- TODO: Document keys
-                    init_selection = leader .. "ss",
-                    node_incremental = leader .. "si", 
-                    scope_incremental = leader .. "sc", 
-                    node_decremental = leader .. "sd", 
-                }, 
-            }, 
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local leader = require("config.keymap").leader
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"vim",
+				"lua",
+				"vimdoc",
+				"query",
+				"html",
+				"css",
+				"java",
+				"markdown",
+				"markdown_inline",
+				"json",
+				"angular",
+				"scss",
+				"javascript",
+				"typescript",
+				"tsx",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					-- TODO: Document keys
+					init_selection = leader .. "ss",
+					node_incremental = leader .. "si",
+					scope_incremental = leader .. "sc",
+					node_decremental = leader .. "sd",
+				},
+			},
+		})
+	end,
 }
