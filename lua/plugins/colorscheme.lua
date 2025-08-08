583521c2857078446ed8da52964709bb8240ef53
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2025-08-08 09:16:44 -0300

    allowing transparency in the color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,27 @@
+local keymap = require("config.keymap")
+
 return {
 	"olimorris/onedarkpro.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
+		local onedarkpro = require("onedarkpro")
+		onedarkpro.setup({
+			options = {
+				transparency = true,
+			},
+		})
+
 		vim.cmd("colorscheme onedark")
+
+		keymap.map(keymap.normalMode, keymap.leader .. "tt", function()
+			local onedarkpro_config = require("onedarkpro.config")
+			onedarkpro.setup({
+				options = {
+					transparency = not onedarkpro_config.options.transparency,
+				},
+			})
+			vim.cmd("colorscheme onedark")
+		end, { desc = "Toggle background transparency" })
 	end,
 }
