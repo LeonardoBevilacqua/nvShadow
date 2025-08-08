c23bdfc7fd99962b87a4cb395c5a2b1ddd294c12
Leonardo Almeida Bevilacqua <lbevilacqua@lenovo.com>
2025-08-08 09:16:18 -0300

    adding new tip about search and replace

--- plugin/vim-cheatsheet.lua
+++ plugin/vim-cheatsheet.lua
@@ -36,6 +36,10 @@ local function show_vim_cheatsheet()
 			title = "Reverse lines",
 			description = "Use the command `:global/^/move 0` or the short version `:g/^/m0`",
 		},
+		{
+			title = "Search and replace selection",
+			description = "Use `/` to search the text to be replaced, and use `:%s//new-string` to replace the last searched pattern",
+		},
 	}
 	for _, tip in ipairs(tips) do
 		table.insert(lines, format_tip(tip))
