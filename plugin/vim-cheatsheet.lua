b128d61495499fefb9e5ece3123ca31e8718e7c2
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-03-04 15:29:25 -0300

    adding new tip in vim cheatsheet

--- plugin/vim-cheatsheet.lua
+++ plugin/vim-cheatsheet.lua
@@ -1,6 +1,7 @@
 ---@class VimTip
 ---@field title string
 ---@field description string
+---@field details? string[]
 
 local state = {
 	floating = {
@@ -20,6 +21,10 @@ local function format_tip(tip)
 	end
 
 	return table.concat(parts, "  ")
+end
+---@param detail string
+local function format_detail(detail)
+	return string.format("\t- %s", detail)
 end
 
 local function show_vim_cheatsheet()
@@ -44,9 +49,26 @@ local function show_vim_cheatsheet()
 			title = "Terminal command history",
 			description = "In terminal, enter command mode (`<Esc>`) and press `/`, type the command to search and press enter. Use `n` and `N` to navigate",
 		},
+		{
+			title = "Search in buffer and send to quickfix",
+			description = "Use `vimgrep` command to search and check in quickfix",
+			details = {
+				"Run the search command: In normal mode, execute the following command: `:vimgrep /pattern/ %`",
+				"`/pattern/` is the regular expression you want to search for.",
+				"`%` is a shortcut that expands to the name of the current file.",
+				"",
+				"Open the quickfix window: The search results will automatically populate the quickfix list. To open the quickfix window to view the results, use:",
+				"`:copen or the shorter version `:cope`",
+			},
+		},
 	}
 	for _, tip in ipairs(tips) do
 		table.insert(lines, format_tip(tip))
+		if tip.details then
+			for _, detail in pairs(tip.details) do
+				table.insert(lines, format_detail(detail))
+			end
+		end
 	end
 
 	if not vim.api.nvim_win_is_valid(state.floating.win) then
