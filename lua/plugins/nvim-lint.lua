27545e15602660382db1a0c3160d7e8ae933354a
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-07 09:08:41 -0300

    updating formatter and adding autocmd to java file

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -7,5 +7,14 @@ return {
 		}
 		local checkstyle = require("lint").linters.checkstyle
 		checkstyle.config_file = vim.fn.getcwd() .. package.config:sub(1, 1) .. "checkstyle.xml"
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			desc = "Try lint when saving a java file",
+			group = vim.api.nvim_create_augroup("java-lint", { clear = true }),
+			pattern = "*.java",
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
 	end,
 }
