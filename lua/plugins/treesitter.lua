85e1d691585c0bc2da07d90ccd634b1706fd4c53
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-09 13:18:31 -0300

    updating nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,45 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	version = "main",
 	build = ":TSUpdate",
 	config = function()
-		local leader = require("config.keymap").leader
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"c",
-				"vim",
-				"lua",
-				"vimdoc",
-				"query",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"json",
-				"angular",
-				"scss",
-				"javascript",
-				"typescript",
-				"tsx",
-				"angular",
-				"python",
-				"svelte",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					-- TODO: Document keys
-					init_selection = leader .. "ss",
-					node_incremental = leader .. "si",
-					scope_incremental = leader .. "sc",
-					node_decremental = leader .. "sd",
-				},
-			},
+		local nvim_treesitter = require("nvim-treesitter")
+		nvim_treesitter.install({
+			"c",
+			"vim",
+			"lua",
+			"vimdoc",
+			"query",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"json",
+			"angular",
+			"scss",
+			"javascript",
+			"typescript",
+			"tsx",
+			"angular",
+			"python",
+			"svelte",
+			"java",
+		})
+		nvim_treesitter.update()
+
+		-- auto-start highlights & indentation
+		vim.api.nvim_create_autocmd("FileType", {
+			desc = "User: enable treesitter highlighting",
+			callback = function(ctx)
+				-- highlights
+				local hasStarted = pcall(vim.treesitter.start) -- errors for filetypes with no parser
+
+				-- indent
+				local noIndent = {}
+				if hasStarted and not vim.list_contains(noIndent, ctx.match) then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
 	end,
 }
