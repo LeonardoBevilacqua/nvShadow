b523d02b9c3ffbccc8e7f19cd992862c989aa6e3
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2026-04-13 10:33:03 -0300

    checking if checkstyle exists

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -2,18 +2,27 @@ return {
 	"mfussenegger/nvim-lint",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		require("lint").linters_by_ft = {
+		local checkstyle_file_path = vim.fn.getcwd() .. package.config:sub(1, 1) .. "checkstyle.xml"
+		local found_checkstyle_file = vim.fn.findfile(checkstyle_file_path)
+		if
+			type(found_checkstyle_file) == "string" and (found_checkstyle_file == nil or found_checkstyle_file == "")
+		then
+			return
+		end
+
+		local lint = require("lint")
+		lint.linters_by_ft = {
 			java = { "checkstyle" },
 		}
-		local checkstyle = require("lint").linters.checkstyle
-		checkstyle.config_file = vim.fn.getcwd() .. package.config:sub(1, 1) .. "checkstyle.xml"
+		local checkstyle = lint.linters.checkstyle
+		checkstyle.config_file = checkstyle_file_path
 
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			desc = "Try lint when saving a java file",
 			group = vim.api.nvim_create_augroup("java-lint", { clear = true }),
 			pattern = "*.java",
 			callback = function()
-				require("lint").try_lint()
+				lint.try_lint()
 			end,
 		})
 	end,
